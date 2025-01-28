36c47deb2289ae9e3dc497d8b62ca19979673570
sondrekol <sondre.kolberg@gmail.com>
2025-01-28 16:54:11 +0100

    boilerplate for uci

---
+++ src/uci.rs
@@ -0,0 +1,38 @@
+
+
+//!For UCI reference
+//https://gist.github.com/DOBRO/2592c6dad754ba67e6dcaec8c90165bf
+
+
+
+//?Idle function for waiting for command????
+fn idle(){
+
+}
+
+
+
+
+/**
+ * preapre_uci_command 
+ * 
+ * 
+ * 
+ */
+fn prepare_uci_command(command:&str){
+
+}
+
+/**
+ * handle_uci_command takes a UCI command as input, performs the appropriate action and returns the appropriate response
+ * 
+ * It assumes that 
+ * 
+ */
+fn handle_uci_command(command:&str) ->  str{
+
+
+
+
+    return "ERROR"
+}
